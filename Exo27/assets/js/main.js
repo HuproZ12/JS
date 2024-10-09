531b28d4be7424a7552b13360315d99e7bb5baee
unknown <hugodtlx@outlook.com>
2024-10-10 00:47:11 +0200

    maj

--- Exo27/assets/js/main.js
+++ Exo27/assets/js/main.js
@@ -1,37 +1,55 @@
 let apiUrl = "https://pokeapi.co/api/v2/";
+let currentPokemonId;
 
-let getPokemon = async () => {
+let getPokemon = async (id) => {
     try {
-        let recherche = document.getElementById("search-input").value;
-
-        let response = await fetch(`${apiUrl}pokemon/${recherche}`);
+        let response = await fetch(`${apiUrl}pokemon/${id}`);
         let data = await response.json();
 
-        let imageUrl = data.sprites.other.home.front_default;
-        let nom = data.name;
-        let poids = data.weight;
-        let taille = data.height;
-        let types = data.types;
-        let capacites = data.abilities;
+        document.getElementById('pokemon-picture').src = data.sprites.other.home.front_default;;
+        document.getElementById('pokemon-name').textContent = `${data.name}`;
+        document.getElementById('pokemon-id').textContent = `#${data.id}`;
+        document.getElementById('pokemon-weight').textContent = `${data.weight / 10} kg`;
+        document.getElementById('pokemon-height').textContent = `${data.height / 10} m`;
 
-        document.getElementById('pokemon-picture').src = imageUrl;
-        document.getElementById('pokemon-name').textContent = nom;
-        document.getElementById('pokemon-weight').textContent = poids;
-        document.getElementById('pokemon-height').textContent = taille;
+        let types = data.types;
         document.getElementById('pokemon-types').innerHTML = "";
         types.forEach(type => {
             let typeItem = document.createElement('li');
             typeItem.textContent = type.type.name;
+            typeItem.classList.add(`type-${type.type.name}`);
             document.getElementById('pokemon-types').appendChild(typeItem);
         });
+
+        let capacites = data.abilities;
         document.getElementById('pokemon-abilities').innerHTML = "";
         capacites.forEach(capacite => {
             let capaciteItem = document.createElement('li');
             capaciteItem.textContent = capacite.ability.name;
             document.getElementById('pokemon-abilities').appendChild(capaciteItem);
         });
+
+        if(data.id > 1){
+            document.getElementById('pokemon-previous').disabled = false;
+        }else{
+            document.getElementById('pokemon-previous').disabled = true;
+        }
+
+        if(data.id < 1025){
+            document.getElementById('pokemon-next').disabled = false;
+        }else{
+            document.getElementById('pokemon-next').disabled = true;
+        }
+
+        currentPokemonId = data.id;
     } catch (error) {
+        console.error("Erreur :", error);
     }
 };
 
-document.getElementById('pokemon-search').addEventListener('submit', getPokemon);
+document.getElementById('pokemon-search').addEventListener('submit', () => {
+    let recherche = document.getElementById("search-input").value;
+    getPokemon(recherche);
+});
+document.getElementById('pokemon-previous').addEventListener('click', () => {getPokemon(currentPokemonId - 1)});
+document.getElementById('pokemon-next').addEventListener('click', () => {getPokemon(currentPokemonId + 1)});
